0c6b775dfae08f2b7aa9d73850eed9a571bfdb78
Adhithya Prabhi <adhithya@neetable.com>
2018-11-30 16:28:45 +0530

    srver files changes

--- index.js
+++ index.js
@@ -12,7 +12,57 @@ var http = require('http');
 app.use(cors());
 
 
-let blogFolderPath = [], blog = null;
+
+var port = process.env.PORT || 1120;
+
+if(process.env.PORT){
+        let blogFolderPath = [], blog = null;
+      let blogList = [];
+
+      traverseDir('static/js/blogs');
+
+      function traverseDir(dir) {
+        fs.readdirSync(dir).forEach(file => {
+          blogFolderPath = path.join(dir, file);
+          if (fs.lstatSync(blogFolderPath).isDirectory()) {
+            traverseDir(blogFolderPath);
+            readFile(blogFolderPath)
+          } else {
+            readFile(blogFolderPath)
+          }
+        });
+      }
+
+      function readFile() {
+        var md = require("markdown").markdown;
+        fs.readFile(blogFolderPath, { encoding: 'utf-8' }, function (err, data) {
+          if (!err) {
+            blog = fm(data);
+            let showData = (JSON.stringify(blog) + ',');
+            setApi(showData);
+          } else {
+            console.log(err);
+          }
+        });
+      }
+
+      function setApi(data) {
+        let dataTest = data.substring(0, data.length - 1);
+        blogList.push(JSON.parse(dataTest));
+
+        app.get('/api/blogs', function (req, res) {
+          res.send(blogList)
+        })
+      }
+
+      app.use(express.static('static/js'));
+
+      app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'static', 'js', '*.js'));
+      })
+}
+else{
+  let blogFolderPath = [], blog = null;
 let blogList = [];
 
 traverseDir('ClientApp/src/blogs');
@@ -51,20 +101,12 @@ function setApi(data) {
   })
 }
 
-
-// if (process.env.NODE_ENV === 'production') {
-//   
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
-//   })
-// }
-
 app.use(express.static('ClientApp/build'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'ClientApp', 'build', 'service-worker.js'));
 })
 
-var port = process.env.PORT || 1120;
+}
+
 app.listen(port);
