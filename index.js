0609220bdd522ca0d781759018e511aa882c4ffc
Adhithya Prabhi <adhithya@neetable.com>
2018-11-30 17:09:36 +0530

    modification refrences

--- index.js
+++ index.js
@@ -60,10 +60,10 @@ var port = process.env.PORT || 1120;
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'ClientApp/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'ClientApp/build', 'index.html'));
   });
 }
 
