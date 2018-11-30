5ebe604ec3133e20d6275be32b410289abadd789
Adhithya Prabhi <adhithya@neetable.com>
2018-11-30 14:53:06 +0530

    new folders

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
+"use strict"
+var express = require('express');
+var app = express();
+var path = require('path');
+var fs = require('fs');
+const cors = require('cors');
+var fm = require('front-matter');
+var marked = require('marked');
+var router = express.Router();
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
+app.use(cors());
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+app.use(express.static('public'))
 
-});
+let blogFolderPath = [], blog = null;
+let blogList = [];
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+traverseDir('ClientApp/src/blogs');
 
-console.log("Server running at http://localhost:%d", port);
+function traverseDir(dir) {
+  fs.readdirSync(dir).forEach(file => {
+    blogFolderPath = path.join(dir, file);
+    if (fs.lstatSync(blogFolderPath).isDirectory()) {
+      traverseDir(blogFolderPath);
+      readFile(blogFolderPath)
+    } else {
+      readFile(blogFolderPath)
+    }
+  });
+}
+
+function readFile() {
+  var md = require("markdown").markdown;
+  fs.readFile(blogFolderPath, { encoding: 'utf-8' }, function (err, data) {
+    if (!err) {
+      blog = fm(data);
+      let showData = (JSON.stringify(blog) + ',');
+      setApi(showData);
+    } else {
+      console.log(err);
+    }
+  });
+}
+
+function setApi(data) {
+  let dataTest = data.substring(0, data.length - 1);
+  blogList.push(JSON.parse(dataTest));
+
+  app.get('/api/blogs', function (req, res) {
+    res.send(blogList)
+  })
+}
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('public/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+  })
+}
+
+app.listen(1114, function () {
+  console.log('app is listening on port 1114');
+})
