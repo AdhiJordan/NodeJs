07480ce90270ee93d238b6bfcfa7c836498df0ef
Adhithya Prabhi <adhithya@neetable.com>
2018-11-30 17:37:20 +0530

    path modified it

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ function setApi(data) {
 
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'ClientApp/build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  res.sendFile(path.join(__dirname + '/ClientApp/build/index.html'))
 })
 
 
